a36e3f861f2cc7f5f0a1a45ecd85fdec6dc040d5
uenotakabon <uenotakabon@gmail.com>
2024-05-22 19:59:26 +0900

    updated

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,3 +4,26 @@ var hamburger = $('.header__hamburgerMenu');
 $('.header__menuBtn').on('click', function () {
   hamburger.toggleClass('header__hamburgerMenu--active');
 });
+
+// タブ
+$('.course__tabListItem').on('click', function(){
+  let index = $('.course__tabListItem').index(this);
+
+  $('.course__tabListItem').removeClass('course__tabListItem--active');
+  $(this).addClass('course__tabListItem--active');
+  $('.course__tabContent').removeClass('course__tabContent--active');
+  $('.course__tabContent').eq(index).addClass('course__tabContent--active');
+});
+
+// スムーススクロール
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var adjust = 0;
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top + adjust;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
